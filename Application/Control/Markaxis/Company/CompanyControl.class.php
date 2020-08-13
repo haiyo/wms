e99d707c31c820d69ed48131c95b12f730f8eb77
Andy Lam <andylwl@gmail.com>
2020-08-13 14:23:35 +0800

    wms-21 No Pay Leave for Payroll Processing

--- Application/Control/Markaxis/Company/CompanyControl.class.php
+++ Application/Control/Markaxis/Company/CompanyControl.class.php
@@ -52,7 +52,7 @@ class CompanyControl {
         $content = file_get_contents( $filename );
         header('Content-Type: ' . $mimeType);
         header('Content-Length: '.strlen( $content ));
-        header('Content-disposition: inline; filename="logo');
+        header('Content-disposition: inline; filename="logo.jpg');
         header('Cache-Control: public, must-revalidate, max-age=0');
         header('Pragma: public');
         header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
